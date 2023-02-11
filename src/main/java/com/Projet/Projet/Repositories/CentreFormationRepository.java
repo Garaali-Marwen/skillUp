6b2f73bdf0cdb4fa054ddd01b23bd6cabf54497f
Ali-Ben-Maouia <alibenmaouia111@gmail.com>
2023-02-11 22:13:14 +0100

    ali test1

---
+++ src/main/java/com/Projet/Projet/Repositories/CentreFormationRepository.java
@@ -0,0 +1,12 @@
+package com.Projet.Projet.Repositories;
+
+import com.Projet.Projet.Entities.CentreFormation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CentreFormationRepository extends JpaRepository<CentreFormation, Long> {
+  CentreFormation findCentreFormationById(Long id);
+
+    void deleteById(Long id);
+}
---
+++ src/main/java/com/Projet/Projet/Services/ServicesImplementation/CentreFormationImpl.java
@@ -0,0 +1,38 @@
+package com.Projet.Projet.Services.ServicesImplementation;
+
+import com.Projet.Projet.Entities.CentreFormation;
+import com.Projet.Projet.Repositories.CentreFormationRepository;
+import com.Projet.Projet.Services.CentreFormationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CentreFormationImpl implements CentreFormationService {
+    @Autowired
+    private  CentreFormationRepository centreFormationRepository;
+    @Override
+    public CentreFormation addCentre(CentreFormation centreFormation) {
+        return centreFormationRepository.save(centreFormation);
+    }
+
+    @Override
+    public List<CentreFormation> getAllCentreFormation() {
+        return centreFormationRepository.findAll();
+    }
+
+    @Override
+    public void deleteCentreFormation(final Long id) {
+
+            centreFormationRepository.deleteById(id);
+
+    }
+
+    @Override
+    public CentreFormation getCentreFormationById(Long id) {
+        return centreFormationRepository.findCentreFormationById(id);
+    }
+
+
+}
