090f3c4d07390442978063852f24e3e1fb52e027
Ahmed Ayadi <97383899+ayadi-ahmed@users.noreply.github.com>
2023-02-11 12:08:37 +0100

    first-commit-ahmed

--- src/main/java/com/Projet/Projet/Controller/FormationController.java
+++ src/main/java/com/Projet/Projet/Controller/FormationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/formation")
+@RequestMapping("/api/formation")
 public class FormationController {
 
     @Autowired
