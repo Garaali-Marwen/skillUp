9186e118968abce996909c8a5ff28f28360ea0df
Garaali-Marwen <garaali.marwen@gmail.com>
2023-02-13 23:23:12 +0100

    Crud Categories

---
+++ src/main/java/com/Projet/Projet/Controller/CategorieController.java
@@ -0,0 +1,41 @@
+package com.Projet.Projet.Controller;
+
+import com.Projet.Projet.Entities.Categorie;
+import com.Projet.Projet.Services.CategorieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categorie")
+public class CategorieController {
+    @Autowired
+    private CategorieService categorieService;
+
+    @GetMapping("")
+    public List<Categorie> getAllCategories(){
+        return categorieService.getAllCategories();
+    }
+    @GetMapping("/{id}")
+    public Categorie getCategorieById(@PathVariable("id") Long categorieId){
+        return categorieService.getCategorieById(categorieId);
+    }
+    @PostMapping("/add")
+    public Categorie addCategorie(@RequestBody Categorie categorie){
+        return categorieService.addCategorie(categorie);
+    }
+    @PutMapping("/update")
+    public Categorie updateCategorie(@RequestBody Categorie categorie){
+        return categorieService.updateCategorie(categorie);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteCategorie(@PathVariable("id") Long categorieId){
+        categorieService.deleteCategorie(categorieId);
+    }
+    @GetMapping("/formation/{fid}/categorie/{cid}")
+    public Categorie addFormationToCategorie(@PathVariable("fid") Long formationId, @PathVariable("cid") Long categorieId){
+        return categorieService.addFormationToCategorie(formationId,categorieId);
+    }
+
+}
---
+++ src/main/java/com/Projet/Projet/Services/CategorieService.java
@@ -0,0 +1,15 @@
+package com.Projet.Projet.Services;
+
+import com.Projet.Projet.Entities.Categorie;
+
+import java.util.List;
+
+public interface CategorieService {
+
+    Categorie addCategorie(Categorie categorie);
+    Categorie getCategorieById(Long categorieId);
+    List<Categorie> getAllCategories();
+    void deleteCategorie(Long categorieId);
+    Categorie updateCategorie(Categorie categorie);
+    Categorie addFormationToCategorie(Long formationId, Long categorieId);
+}
