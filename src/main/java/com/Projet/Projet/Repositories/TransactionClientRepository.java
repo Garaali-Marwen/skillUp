2d176a7b9237cf822931c5e58477cc1b5013b612
Ahmed Ayadi <ayadi.ahmed4@gmail.com>
2023-04-27 20:29:16 +0100

    checkout-client

--- src/main/java/com/Projet/Projet/Repositories/TransactionClientRepository.java
+++ src/main/java/com/Projet/Projet/Repositories/TransactionClientRepository.java
@@ -4,6 +4,10 @@ import com.Projet.Projet.Entities.TransactionClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionClientRepository extends JpaRepository<TransactionClient,Long> {
+    List<TransactionClient> findTransactionClientByClient_Id(Long clientId);
+
 }
--- src/main/java/com/Projet/Projet/Repositories/UserRepository.java
+++ src/main/java/com/Projet/Projet/Repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.Projet.Projet.Repositories;
 
+import com.Projet.Projet.Entities.TransactionClient;
 import com.Projet.Projet.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
